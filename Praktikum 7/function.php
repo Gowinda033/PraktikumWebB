a85d19cd21952357a2225feeceacc88f5d62a470
Gowinda033 <71446856+Gowinda033@users.noreply.github.com>
2020-11-24 23:03:52 +0800

    Add files via upload

---
+++ Praktikum 7/function.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Praktikum 7</title>
+    </head>
+    <body>
+    <?php
+        
+            $nama = $_POST['nama'];
+            $nim = $_POST['nim'];
+            $tugas = $_POST['tugas'];
+            $uts = $_POST['uts'];
+            $uas = $_POST['uas'];
+
+            $akhir = (($tugas+$uts+$uas)/3);
+    ?>
+        <h1>Nilai Akhir Mahaiswa</h1>
+            Nama = <?php echo $nama ?><br>
+            NIM  = <?php echo $nim ?><br><br>
+            <div class="">
+                Nilai Tugas Anda = <?php echo $tugas ?><br>
+            
+                Nilai UTS Anda  = <?php echo $uts ?><br>
+            
+                Nilai UAS Anda  = <?php echo $uas ?><br>
+            
+                <h2> Nilai Akhir Anda = <?php echo $akhir ?></h2>
+            </div>
+            <div class="">
+                    <h3>
+                        <?php
+                            if($akhir >= 80){
+                                echo "Anda Dinyatakan Lulus Dengan Predikat A";
+                            }
+                            elseif($akhir >= 70){
+                                echo "Anda Dinyatakan Lulus Dengan Predikat B";
+                            }
+                            elseif($akhir >= 60){
+                                echo "Anda Dinyatakan Lulus Dengan Predikat C";
+                            }
+                            else{
+                                echo "Maaf Anda Dinyatakan Tidak Lulus";
+                            }
+                        ?>
+                    </h3>  
+            </div>
+    <?php
+      
+    ?>
+    </body>
+</html>
